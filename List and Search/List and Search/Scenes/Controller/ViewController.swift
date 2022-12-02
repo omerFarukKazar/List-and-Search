92f2a8937810cd9aa58c58a1b483273155abb562
omerfkzr <omer.kazar22@gmail.com>
2022-12-02 21:51:32 +0300

    Tableview protocols conformed with dummy data.

--- List and Search/List and Search/Scenes/Controller/ViewController.swift
+++ List and Search/List and Search/Scenes/Controller/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,
+                      UITableViewDelegate,
+                      UITableViewDataSource{
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        listTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        listTableView.delegate = self
+        listTableView.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+         fatalError("UITableViewCell not found.")
+        }// UITableViewCell() olarak da oluşturabilirdik ancak o şekilde talbeView'den bağımsız olacağı için, didSelectRow gibi methodlarda sıkıntı çıkabilmesi ihtimaline karşın tableView üzerinden oluşturduk.
+        cell.textLabel?.text = "Row - \(indexPath.row)"
+        return cell
+    }
 
 }
 
