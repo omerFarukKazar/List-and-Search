72aceaaa135099af8f23275743331ab2e599b9c5
omerfkzr <omer.kazar22@gmail.com>
2022-12-02 13:11:40 +0300

    Project file created, Project setttings changed.

---
+++ List and Search/List and Search/Scenes/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  List and Search
+//
+//  Created by Ömer Faruk Kazar on 2.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
